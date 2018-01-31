1cdbcd4231b63d4efd1773c2a3e22fee817a938a
Syafiq Kamarul Azman <syafiqkamarulazman@gmail.com>
2018-01-31 22:39:12 +0400

    Updated home layout for openday

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,7 +1,15 @@
-Webcam.set({
-    width: 320,
-    height: 240,
-    image_format: 'jpeg',
-    jpeg_quality: 90
-});
-Webcam.attach('#camera');
+if (window.location.pathname === "/") {
+    Webcam.set({
+        width: 480,
+        height: 360,
+        image_format: 'png'
+    });
+    Webcam.attach('#camera');
+
+    document.getElementById('snap').addEventListener('click', function() {
+        Webcam.snap(function(data) {
+            document.getElementById('image-data').value = data;
+            document.getElementById('upload-form').submit();
+        });
+    });
+}
